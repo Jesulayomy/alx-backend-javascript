9a412459826b59cf31700eb192ea19cfc6cc945b
Jesulayomy <jesulayomi081@gmail.com>
2023-07-27 09:47:28 +0000

    Class method and declarations

--- 0x01-ES6_promise/6-main.js
+++ 0x01-ES6_promise/6-main.js
@@ -1,3 +1,5 @@
 import handleProfileSignup from './6-final-user';
 
 console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg"));
+console.log(handleProfileSignup("Boba", "Donkey", "boba_donkey.jpg"));
+console.log(handleProfileSignup("Boobi", "Dyane", "boobi_dyane.jpg"));
---
+++ 0x02-ES6_classes/README.md
@@ -0,0 +1,9 @@
+# ES6 Classes
+
+- Classes
+ - Methods
+ - Static Method
+ - Extend
+
+- MetaProgramming
+ - Symbols
