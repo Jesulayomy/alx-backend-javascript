013b7f93c30f4c9304251ba499ecc4b4b4fa5492
Jesulayomy <jesulayomi081@gmail.com>
2023-07-27 03:51:50 +0000

    Rewrite Async Function

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,16 +4,12 @@ import uploadPhoto from './5-photo-reject';
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then(([signResult, uploadResult]) => [
-      {
-        status: signResult.status,
-        value: signResult.status === 'fulfilled' ? String(signResult.value) : String(signResult.reason),
-      },
-      {
-        status: uploadResult.status,
-        value:
-          uploadResult.status === 'fulfilled'
-            ? String(uploadResult.value) : String(uploadResult.reason),
-      },
-    ]);
+    .then((result) => (
+      result.map((structure) => ({
+        status: structure.status,
+        value: structure.status === 'fulfilled'
+          ? structure.value
+          : String(structure.reason),
+      }))
+    ));
 }
