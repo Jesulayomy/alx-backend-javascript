e6dc0dc5e75962bb062708e2ec4cfa241043df73
Jesulayomy <jesulayomi081@gmail.com>
2023-07-27 16:55:10 +0000

    Classes and extendions

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,36 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(price) {
+    if (typeof price === 'number') {
+      this._amount = price;
+    } else {
+      throw new TypeError('Amount must be a number');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(sign) {
+    if (sign instanceof Currency) {
+      this._currency = sign;
+    } else {
+      throw new TypeError('Currency must be a currency');
+    }
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+}
