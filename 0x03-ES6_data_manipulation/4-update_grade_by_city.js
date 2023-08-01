34b0e65320a9f018c06afd93a2b23c364d02f634
Aina Jesulayomi <113533393+Jesulayomy@users.noreply.github.com>
2023-08-01 23:38:05 +0100

    Rewrite code to use instance checks

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,17 @@
 export default function updateStudentGradeByCity(listStudents, city, newGrades) {
-  return listStudents.filter((student) => student.location === city)
-    .map((student) => {
-      for (const newGrade of newGrades) {
-        if (student.id === newGrade.studentId) {
-          return { ...student, grade: newGrade.grade };
-	}
-      }
-      return { ...student, grade: 'N/A' };
-    });
+  const defaultGrade = { grade: 'N/A' };
+
+  if (listStudents instanceof Array) {
+    return students
+      .filter((student) => student.location === city)
+      .map((student) => ({
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: (newGrades
+          .filter((grade) => grade.studentId === student.id)
+          .pop() || defaultGrade).grade,
+      }));
+  }
+  return [];
 }
