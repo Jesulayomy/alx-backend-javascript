59bbbce2e6da7d65ca22e8351e793031c211f04c
Aina Jesulayomi <113533393+Jesulayomy@users.noreply.github.com>
2023-08-01 18:47:48 +0100

    Update 4-update_grade_by_city to fix comparison operator

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,7 +2,7 @@ export default function updateStudentGradeByCity (listStudents, city, newGrades)
   return listStudents.filter((student) => student.location === city)
     .map((student) => {
       for (const newGrade of newGrades) {
-        if (student.id == newGrade.studentId) {
+        if (student.id === newGrade.studentId) {
           return { ...student, grade: newGrade.grade };
 	}
       }
